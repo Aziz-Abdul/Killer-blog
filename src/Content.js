4473493775b31d255ea904ce0cdccb61e059f4c3
Abdul-Aziz <54721014+Aziz-Abdul@users.noreply.github.com>
2020-10-16 16:48:33 +0000

    second commit

---
+++ src/Content.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Content() {
+    return (
+        <div>
+             <span>
+                <h1>Abdul-Aziz Abdullah</h1>
+    
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRjVOMDwqOGDZ95Mj_FXLklhd0DLN8T9TKLAQ&usqp=CAU.jpg"/>
+    
+                <p>Hi.i am Abdul-Aziz Abdullah.I am Tall ,handsome , extremely gentle,Intelligent,God fearing , Resourceful. I am your quintessential archetype gentleman. currently ,i am feeling miserable ,down and out with a sunken spirit. I am currently jobless and broke due to bad decisions,indiscretion etc but i am trying my hands on a whole lot of things including computer programming which i am keen on taking to the next level.I am made several mistakes in life including my last as an Operations Manager,regretably so.I have learnt many lessons in this short of life thus far and i have picked up my pieces ,dusted myself and i am ready to ride again as a better man.May the Almighty Allah help us all. </p>
+    
+                <p>I might not have been successful in my previous adventures ,but hey mind you i have a solid Resume. find below intro the excerpts of my Resume,<strong><q>Share my knowledge in Best Practices in Telecommunications Mobile financial Services/Financial Technology, Telecommunications Operations and Innovations Management; through the promotion of a culture of Continual Improvement. Additionally, to provide quality service in order to realize higher returns on investment through effective decision-making, based on the analysis of data and information in a safe working environment where employees will develop their capabilities and grow through education and experience</q></strong>.</p>
+             </span>
+    
+        <h3>Awesome Places i would love to work</h3>
+        <span><strong><i>Blue Chip Companies are global brands and every prospective employee would love to be associated with.</i></strong>Some of these global brands includes but not limited to the following;Apple,MTN Group, Andela ,Bank of Ghana,Alphabet,Microsoft Group and JP Morgan Chase
+        </span>
+        <h3>Some of My Favorite Tourism Destinations Earth</h3>
+        
+        <span>Mecca-Saudi Arabia, Santorini-Greece,Bali-Indonesia,Seville-Spain,The Niagara Falls-Himalayans,Eastern Europe and The Balkans,CopoCabana Beach and The Amazons(Brazil) ,Some Selected Beaches and Countries in Afric</span>
+            
+        <h3> Social Media Pages</h3>
+       <span> I have quite few you can reach me on. linkedin and Facebook are my favorites though. Kindly find links below:</span>
+    
+        <ul><li><a href="https://www.linkedin.com/in/abdul-aziz-abdullah-5357b534/">Linkedin</a></li></ul>
+    
+       <ul> <li><a href="https://web.facebook.com/babykilla"> Facebook</a></li></ul>
+     </div>
+
+    );
+}
+
+export default Content;
